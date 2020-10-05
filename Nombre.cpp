381fbdec7dfc859b9b37fa39a18a8dc53623dac1
Aldair Angeles Vásquez <71864416+AldairAV@users.noreply.github.com>
2020-10-05 14:40:51 -0500

    Create Nombre.cpp
    Nombre completo

---
+++ Nombre.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h> //stdio -> Standard Input/Output
+
+int main(){
+	/*My first
+	 * Program
+	 * in C
+	 */
+	printf("Hola mundo, me llamo: ");
+	printf("Aldair Angeles Vásquez " )
+	//Comment
+	return 0;
+}
